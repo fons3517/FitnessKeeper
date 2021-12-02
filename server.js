d6d1268a58fb4762e8e42990eb325b895e359f97
Alfonso Robles <fons3517@gmail.com>
2021-12-01 23:52:56 -0600

    changed server.js db to workout to match seed.js db name

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-  "mongodb://localhost/FitnessData",
+  "mongodb://localhost/workout",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
